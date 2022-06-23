2e1bc1d090266c64e2079d381cd202c74e2df07f
Nikita Bykov <bns.6587@gmail.com>
2022-06-23 23:49:53 +0300

    prepare for voice

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,39 @@
 mod red_alert_handler;
 
+use std::env;
+use guard::*;
 use red_alert_handler::*;
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 use serenity::*;
+
+async fn user_name(ctx: &Context, user_id: &UserId) -> Option<String> {
+    if let Some(cached_user_name) = ctx.cache.user(user_id).await.map(|u| u.name) {
+        return Some(cached_user_name);
+    }
+    if let Ok(received_user_name) = ctx.http.get_user(user_id.0).await.map(|u| u.name) {
+        return Some(received_user_name);
+    }
+    None
+}
+
+fn is_sub<T: PartialEq>(first: &Vec<T>, second: &Vec<T>) -> bool {
+    if second.len() == 0 {
+        return false;
+    }
+    let mut index: usize = 0;
+    for element in first {
+        if &second[index] == element {
+            index += 1;
+        } else {
+            index = 0;
+        }
+        if second.len() == index {
+            return true;
+        }
+    }
+    false
+}
 
 struct Handler {
     red_alert_handler: RedAlertHandler,
@@ -12,10 +42,110 @@ struct Handler {
 impl Default for Handler {
     fn default() -> Self {
         Self {
-            red_alert_handler: RedAlertHandler::new(
-                std::collections::HashSet::from(["код красный ".to_string(), "код к ".to_string()]),
-                std::collections::HashSet::from([UserId(224181375912116227)]),
-            ),
+            red_alert_handler: RedAlertHandler,
+        }
+    }
+}
+
+impl Handler {
+    async fn process_red_alert(
+        &self,
+        ctx: &Context,
+        guild: &Guild,
+        author_user_id: UserId,
+        target_users_ids: Vec<UserId>,
+    ) -> String {
+        let red_alert_handler = &self.red_alert_handler;
+
+        let red_alert_result = red_alert_handler
+            .handle(&ctx, &guild, target_users_ids)
+            .await;
+
+        match red_alert_result.len() {
+            0 => {
+                if let Some(result) = red_alert_handler
+                    .handle(&ctx, &guild, vec![author_user_id])
+                    .await
+                    .get(&author_user_id)
+                {
+                    match result {
+                        RedAlertDeportationResult::Deported => format!("ВИЖУ ТЫ ЗАБЫЛ УКАЗАТЬ ЦЕЛЬ ДЛЯ КРАСНОГО КОДА, НИЧЕГО... ШМАЛЬНЕМ В ТЕБЯ! (ИСПОЛЬЗУЙ ТЕГИ) ПРИНЯТО К ИСПОЛНЕНИЮ!"),
+                        RedAlertDeportationResult::NotFound => format!(":face_with_monocle: ПОЛЬЗУЙСЯ ТЕГАМИ, И ЛУЧШЕ НЕ ЗАХОДИ В КАНАЛ, А ТО КИКНУ С ТАКИМИ ПРИКОЛАМИ! Пшшшш..."),
+                        RedAlertDeportationResult::Error(_) => format!("СЛОМАЛСЯ ПОКА ПЫТАЛСЯ ТЕБЯ КИКНУТЬ ЧТО НЕПРАВИЛЬНОЕ ИСПОЛЬЗОВАНИЕ, КАК ВСЕГДА КОД ГОВНА! ОТМЕНА! Пшшшш...")
+                    }
+                } else {
+                    format!("КАНДИДАТ НА КОД КРАСНЫЙ НЕ УКАЗАН! ОТМЕНА ОПЕРАЦИИ! Пшшшш...")
+                }
+            }
+            1 => {
+                if let Some((user_id, deport_status)) = red_alert_result.iter().next() {
+                    let user_name = user_name(&ctx, user_id)
+                        .await
+                        .unwrap_or("\"НЕИЗВЕСТНЫЙ ТИП\"".to_string());
+                    match deport_status {
+                        RedAlertDeportationResult::Deported => format!("КОД КРАСНЫЙ ПОДТВЕРЖДЕН! АНТИКРИНЖ ОРУЖИЕ ИСПОЛЬЗОВАНО ПРОТИВ {user_name}!!! 0)00))00"),
+                        RedAlertDeportationResult::NotFound => {
+                            if let Some(result) = red_alert_handler.handle(
+                                &ctx,
+                                &guild,
+                                vec![author_user_id]
+                            )
+                                .await
+                                .get(&author_user_id)
+                            {
+                                match result {
+                                    RedAlertDeportationResult::Deported => format!("В КАНАЛЕ НЕТ ЧЕЛА ДЛЯ КОДА КРАСНОГО, ЗНАЧИТ У ТЕБЯ БЕДЫ С БОШКОЙ, КОД КРАСНЫЙ НА ТЕБЯ!"),
+                                    RedAlertDeportationResult::NotFound => format!("ДОФИГА УМНЫЙ ВИЖУ? В КАНАЛЕ НЕТ ЧЕЛА ДЛЯ КОДА КРАСНОГО, ЖАЛЬ ТЕБЯ В КАНАЛЕ НЕТУ, ТАК БЫ ТЕБЯ ШМАЛЬНУЛ КОДОМ КРАСНЫМ! ОТМЕНА! Пшшшш..."),
+                                    RedAlertDeportationResult::Error(_) => format!("ХОТЕЛ ШМАЛЬНУТЬ В ТЕБЯ ЗА ТО ЧТО ТЫ ПЫТАЛСЯ КИКНУТЬ ТОГО КОГО НЕТ, НО Я СЛОМАЛСЯ! Пшшшш...")
+                                }
+                            } else {
+                                format!("КАНДИДАТ НА КОД КРАСНЫЙ НЕ НАЙДЕН! ОТМЕНА ОПЕРАЦИИ! Пшшшш...")
+                            }
+                        }
+                        RedAlertDeportationResult::Error(_) => {
+                            format!("АУЧ, МАСЛИНУ ПОЙМАЛ, ОШИБКА В СИСТЕМЕё0))")
+                        }
+                    }
+                } else {
+                    format!("ШТО ПРОИСХОДИТ))0)0 ТЫ МЕНЯ ПОЧТИ СЛОМАЛ))) НО САСАЙ)")
+                }
+            }
+            _ => {
+                let mut some_kicked = false;
+                let mut result_strings = Vec::new();
+                for (user_id, deport_status) in red_alert_result {
+                    let user_name = user_name(&ctx, &user_id)
+                        .await
+                        .unwrap_or("\"НЕИЗВЕСТНЫЙ ТИП\"".to_string());
+                    let deport_status = match deport_status {
+                        RedAlertDeportationResult::Deported => {
+                            some_kicked = true;
+                            "ИСПОЛНЕНО"
+                        }
+                        RedAlertDeportationResult::NotFound => "НЕ В КАНАЛЕ",
+                        RedAlertDeportationResult::Error(_) => "ОШИБКА (ПРОЧНЫЙ СУ*А)",
+                    };
+                    result_strings.push(format!("{user_name} СТАТУС: {deport_status}"))
+                }
+                let result_string = result_strings.join("\n");
+                if some_kicked {
+                    format!("ОУ, МАССОВЫЙ КОД КРАСНЫЙ? СТАТУС ВЫКОСА КРИНЖОВИКОВ:\n{result_string}")
+                } else {
+                    if let Some(result) = red_alert_handler
+                        .handle(&ctx, &guild, vec![author_user_id])
+                        .await
+                        .get(&author_user_id)
+                    {
+                        match result {
+                            RedAlertDeportationResult::Deported => format!("МАССОВЫЙ КОД КРАСНЫЙ ШТУКА ОПАСНАЯ, ТАК КАК ПО РАЗНЫМ ПРИЧИНАМ Я НИКОГО НЕ КИКНУЛ, КИКНУ ТЕБЯ )В)В)))0"),
+                            RedAlertDeportationResult::NotFound => format!("ЖАЛЬ ТЕБЯ НЕ МОГУ ПРШИТЬ ЗА ЛОЖНЫЙ КОД КРАСНЫЙ! ОТМЕНА Пшшшш..."),
+                            RedAlertDeportationResult::Error(_) => format!("ХОТЕЛ ШМАЛЬНУТЬ В ТЕБЯ ЗА ЛОЖНЫЙ КОД КРАСНЫЙ, НО САМ ОБО****СЯ! Пшшшш...")
+                        }
+                    } else {
+                        format!("ПОЛНЫЙ ПРОВАЛ КОДА КРАСНОГО! ОТМЕНА ОПЕРАЦИИ Пшшшш...")
+                    }
+                }
+            }
         }
     }
 }
@@ -23,112 +153,25 @@ impl Default for Handler {
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        let red_alert_handler = &self.red_alert_handler;
-        async fn user_name(ctx: &Context, user_id: &UserId) -> Option<String> {
-            if let Some(cached_user_name) = ctx.cache.user(user_id).await.map(|u| u.name) {
-                return Some(cached_user_name);
-            }
-            if let Ok(received_user_name) = ctx.http.get_user(user_id.0).await.map(|u| u.name) {
-                return Some(received_user_name);
-            }
-            None
-        }
-        let answer_msg = match red_alert_handler.answer(&ctx, &msg).await {
-            RedAlertHandlerAnswer::Empty => format!(""),
-            RedAlertHandlerAnswer::NotGuildChat => {
-                format!("Инструкцию читай, валенок...")
-            }
-            RedAlertHandlerAnswer::BlockedUser(user_id) => {
-                if let Some(user_name) = user_name(&ctx, &user_id).await {
-                    format!("КОРОЛЬ КРИНЖА НЕ ИМЕЕТ ДОСТУПА К ОРУЖИЮ! ОТМЕНА ОПЕРАЦИИ! {user_name} ИДЕТ ДОМОЙ! 0)0000")
-                } else {
-                    format!("ТЫ ВООБЩЕ КТО ТАКОЙ? ЭНИВЕЙ ТЕБЕ НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ ОРУЖИЕ, ИДИ ПОЧИЛЬ! 0)0000")
-                }
-            }
-            RedAlertHandlerAnswer::DeportationResult(result) => {
-                match result.len() {
-                    0 => {
-                        if let Some(suicide_deport_status) = red_alert_handler
-                            .suicide_author_if_possible(&ctx, &msg)
-                            .await
-                        {
-                            match suicide_deport_status {
-                            RedAlertDeportationUserResult::Deported => format!("ВИЖУ ТЫ ЗАБЫЛ УКАЗАТЬ ЦЕЛЬ ДЛЯ КРАСНОГО КОДА, НИЧЕГО... ШМАЛЬНЕМ В ТЕБЯ! (ИСПОЛЬЗУЙ ТЕГИ) ПРИНЯТО К ИСПОЛНЕНИЮ!"),
-                            RedAlertDeportationUserResult::NotFound => format!(":face_with_monocle: ПОЛЬЗУЙСЯ ТЕГАМИ, И ЛУЧШЕ НЕ ЗАХОДИ В КАНАЛ, А ТО КИКНУ С ТАКИМИ ПРИКОЛАМИ! Пшшшш..."),
-                            RedAlertDeportationUserResult::Error(_) => format!("СЛОМАЛСЯ ПОКА ПЫТАЛСЯ ТЕБЯ КИКНУТЬ ЧТО НЕПРАВИЛЬНОЕ ИСПОЛЬЗОВАНИЕ, КАК ВСЕГДА КОД ГОВНА! ОТМЕНА! Пшшшш...")
-                        }
-                        } else {
-                            format!("КАНДИДАТ НА КОД КРАСНЫЙ НЕ УКАЗАН! ОТМЕНА ОПЕРАЦИИ! Пшшшш...")
-                        }
-                    }
-                    1 => {
-                        if let Some((user_id, deport_status)) = result.iter().next() {
-                            let user_name = user_name(&ctx, user_id)
-                                .await
-                                .unwrap_or("\"НЕИЗВЕСТНЫЙ ТИП\"".to_string());
-                            match deport_status {
-                            RedAlertDeportationUserResult::Deported => format!("КОД КРАСНЫЙ ПОДТВЕРЖДЕН! АНТИКРИНЖ ОРУЖИЕ ИСПОЛЬЗОВАНО ПРОТИВ {user_name}!!! 0)00))00"),
-                            RedAlertDeportationUserResult::NotFound => {
-                                if let Some(suicide_deport_status) = red_alert_handler
-                                    .suicide_author_if_possible(&ctx, &msg)
-                                    .await
-                                {
-                                    match suicide_deport_status {
-                                        RedAlertDeportationUserResult::Deported => format!("В КАНАЛЕ НЕТ ЧЕЛА ДЛЯ КОДА КРАСНОГО, ЗНАЧИТ У ТЕБЯ БЕДЫ С БОШКОЙ, КОД КРАСНЫЙ НА ТЕБЯ!"),
-                                        RedAlertDeportationUserResult::NotFound => format!("ДОФИГА УМНЫЙ ВИЖУ? В КАНАЛЕ НЕТ ЧЕЛА ДЛЯ КОДА КРАСНОГО, ЖАЛЬ ТЕБЯ В КАНАЛЕ НЕТУ, ТАК БЫ ТЕБЯ ШМАЛЬНУЛ КОДОМ КРАСНЫМ! ОТМЕНА! Пшшшш..."),
-                                        RedAlertDeportationUserResult::Error(_) => format!("ХОТЕЛ ШМАЛЬНУТЬ В ТЕБЯ ЗА ТО ЧТО ТЫ ПЫТАЛСЯ КИКНУТЬ ТОГО КОГО НЕТ, НО Я СЛОМАЛСЯ! Пшшшш...")
-                                    }
-                                } else {
-                                    format!("КАНДИДАТ НА КОД КРАСНЫЙ НЕ НАЙДЕН! ОТМЕНА ОПЕРАЦИИ! Пшшшш...")
-                                }
-                            }
-                            RedAlertDeportationUserResult::Error(_) => {
-                                format!("АУЧ, МАСЛИНУ ПОЙМАЛ, ОШИБКА В СИСТЕМЕё0))")
-                            }
-                        }
-                        } else {
-                            format!("ШТО ПРОИСХОДИТ))0)0 ТЫ МЕНЯ ПОЧТИ СЛОМАЛ))) НО САСАЙ)")
-                        }
-                    }
-                    _ => {
-                        let mut some_kicked = false;
-                        let mut result_strings = Vec::new();
-                        for (user_id, deport_status) in result {
-                            let user_name = user_name(&ctx, &user_id)
-                                .await
-                                .unwrap_or("\"НЕИЗВЕСТНЫЙ ТИП\"".to_string());
-                            let deport_status = match deport_status {
-                                RedAlertDeportationUserResult::Deported => {
-                                    some_kicked = true;
-                                    "ИСПОЛНЕНО"
-                                }
-                                RedAlertDeportationUserResult::NotFound => "НЕ В КАНАЛЕ",
-                                RedAlertDeportationUserResult::Error(_) => "ОШИБКА (ПРОЧНЫЙ СУ*А)",
-                            };
-                            result_strings.push(format!("{user_name} СТАТУС: {deport_status}"))
-                        }
-                        let result_string = result_strings.join("\n");
-                        if some_kicked {
-                            format!("ОУ, МАССОВЫЙ КОД КРАСНЫЙ? СТАТУС ВЫКОСА КРИНЖОВИКОВ:\n{result_string}")
-                        } else {
-                            if let Some(suicide_deport_status) = red_alert_handler
-                                .suicide_author_if_possible(&ctx, &msg)
-                                .await
-                            {
-                                match suicide_deport_status {
-                                RedAlertDeportationUserResult::Deported => format!("МАССОВЫЙ КОД КРАСНЫЙ ШТУКА ОПАСНАЯ, ТАК КАК ПО РАЗНЫМ ПРИЧИНАМ Я НИКОГО НЕ КИКНУЛ, КИКНУ ТЕБЯ )В)В)))0"),
-                                RedAlertDeportationUserResult::NotFound => format!("ЖАЛЬ ТЕБЯ НЕ МОГУ ПРШИТЬ ЗА ЛОЖНЫЙ КОД КРАСНЫЙ! ОТМЕНА Пшшшш..."),
-                                RedAlertDeportationUserResult::Error(_) => format!("ХОТЕЛ ШМАЛЬНУТЬ В ТЕБЯ ЗА ЛОЖНЫЙ КОД КРАСНЫЙ, НО САМ ОБО****СЯ! Пшшшш...")
-                            }
-                            } else {
-                                format!("ПОЛНЫЙ ПРОВАЛ КОДА КРАСНОГО! ОТМЕНА ОПЕРАЦИИ Пшшшш...")
-                            }
-                        }
-                    }
-                }
-            }
+        if msg.author.bot {
+            return;
         };
-        if !answer_msg.is_empty() {
+        guard!(let Some(guild) = msg.guild(&ctx.cache).await else { return });
+        let message = msg.content.to_lowercase();
+        let message_words: Vec<&str> = message.split(char::is_whitespace).collect();
+        if !is_sub(&message_words, &vec!["код", "красный"]) {
+            return;
+        }
+        if !msg.mention_channels.is_empty() {
+            let channel = &msg.m
+            let channel_name = &channel.name;
+            let _ = msg.channel_id.say(&ctx, format!("ОТСЛЕЖИВАЮ КОД КРАСНЫЙ В КАНАЛЕ: {channel_name}...")).await;
+        } else {
+            let author_id = msg.author.id;
+            let target_users_ids: Vec<UserId> = msg.mentions.iter().map(|u| u.id).collect();
+            let answer_msg = self
+                .process_red_alert(&ctx, &guild, author_id, target_users_ids)
+                .await;
             let _ = msg.channel_id.say(&ctx, answer_msg).await;
         }
     }
@@ -139,8 +182,8 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let token = args.get(1).expect("Missed Discord token in args");
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
     let mut client = Client::builder(&token)
         .event_handler(Handler::default())
         .await
--- src/red_alert_handler.rs
+++ src/red_alert_handler.rs
@@ -1,102 +1,32 @@
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 
-pub enum RedAlertDeportationUserResult {
+pub enum RedAlertDeportationResult {
     Deported,
     NotFound,
     Error(SerenityError),
 }
 
-pub enum RedAlertHandlerAnswer {
-    Empty,
-    NotGuildChat,
-    BlockedUser(UserId),
-    DeportationResult(std::collections::HashMap<UserId, RedAlertDeportationUserResult>),
-}
-
-pub struct RedAlertHandler {
-    prefix_key_words: std::collections::HashSet<String>,
-    users_ids_black_list: std::collections::HashSet<UserId>,
-}
+pub struct RedAlertHandler;
 
 impl RedAlertHandler {
-    pub fn new(
-        prefix_key_words: std::collections::HashSet<String>,
-        users_ids_black_list: std::collections::HashSet<UserId>,
-    ) -> Self {
-        Self {
-            prefix_key_words,
-            users_ids_black_list,
-        }
-    }
-
-    fn is_contains_prefix_key_word(&self, content: &String) -> bool {
-        let lowercase_content = content.to_lowercase();
-        for prefix_key_word in &self.prefix_key_words {
-            let lowercase_prefix_key_word = prefix_key_word.to_lowercase();
-            if lowercase_content.strip_prefix(&lowercase_prefix_key_word) != None {
-                return true;
-            }
-        }
-        false
-    }
-
-    async fn deportation(
+    pub async fn handle<'a>(
+        &self,
         ctx: &Context,
         guild: &Guild,
-        execution_users_ids: std::collections::HashSet<UserId>,
-    ) -> std::collections::HashMap<UserId, RedAlertDeportationUserResult> {
+        target_users_ids: Vec<UserId>,
+    ) -> std::collections::HashMap<UserId, RedAlertDeportationResult> {
         let mut result = std::collections::HashMap::new();
-        for user_id in execution_users_ids {
+        for user_id in target_users_ids {
             if let Some(_voice_state) = guild.voice_states.get(&user_id) {
                 match guild.id.disconnect_member(&ctx, user_id).await {
-                    Ok(_member) => result.insert(user_id, RedAlertDeportationUserResult::Deported),
-                    Err(err) => result.insert(user_id, RedAlertDeportationUserResult::Error(err)),
+                    Ok(_member) => result.insert(user_id, RedAlertDeportationResult::Deported),
+                    Err(err) => result.insert(user_id, RedAlertDeportationResult::Error(err)),
                 };
             } else {
-                result.insert(user_id, RedAlertDeportationUserResult::NotFound);
+                result.insert(user_id, RedAlertDeportationResult::NotFound);
             }
         }
         return result;
-    }
-
-    pub async fn suicide_author_if_possible(
-        &self,
-        ctx: &Context,
-        msg: &Message,
-    ) -> Option<RedAlertDeportationUserResult> {
-        let guild = if let Some(guild) = msg.guild(&ctx).await {
-            guild
-        } else {
-            return None;
-        };
-        Self::deportation(
-            &ctx,
-            &guild,
-            std::collections::HashSet::from([msg.author.id]),
-        )
-        .await
-        .remove(&msg.author.id)
-    }
-
-    pub async fn answer(&self, ctx: &Context, msg: &Message) -> RedAlertHandlerAnswer {
-        if msg.author.bot || !self.is_contains_prefix_key_word(&msg.content) {
-            return RedAlertHandlerAnswer::Empty;
-        }
-
-        let guild = if let Some(guild) = msg.guild(&ctx).await {
-            guild
-        } else {
-            return RedAlertHandlerAnswer::NotGuildChat;
-        };
-
-        if self.users_ids_black_list.contains(&msg.author.id) {
-            return RedAlertHandlerAnswer::BlockedUser(msg.author.id);
-        }
-
-        let mentions = std::collections::HashSet::from_iter(msg.mentions.iter().map(|u| u.id));
-        let deportation_result = Self::deportation(&ctx, &guild, mentions).await;
-
-        RedAlertHandlerAnswer::DeportationResult(deportation_result)
     }
 }

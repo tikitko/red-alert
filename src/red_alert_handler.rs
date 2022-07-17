72e2a235a9cc28d9ea14cf7d86e2f2d05b64a29a
Nikita Bykov <bns.6587@gmail.com>
2022-07-17 22:41:43 +0300

    kick broke possible fix

--- src/red_alert_handler.rs
+++ src/red_alert_handler.rs
@@ -14,14 +14,21 @@ impl RedAlertHandler {
     pub async fn handle<'a>(
         &self,
         ctx: &Context,
-        guild: &Guild,
+        guild_id: GuildId,
         target_users_ids: Vec<UserId>,
     ) -> std::collections::HashMap<UserId, RedAlertDeportationResult> {
+        let guild = ctx.cache.guild(guild_id).await;
         let mut result = std::collections::HashMap::new();
         for user_id in target_users_ids {
-            if let Some(_voice_state) = guild.voice_states.get(&user_id) {
-                match guild.id.disconnect_member(&ctx, user_id).await {
-                    Ok(_member) => result.insert(user_id, RedAlertDeportationResult::Deported),
+            if {
+                if let Some(guild) = &guild {
+                    guild.voice_states.get(&user_id).is_some()
+                } else {
+                    false
+                }
+            } {
+                match guild_id.disconnect_member(&ctx, user_id).await {
+                    Ok(_) => result.insert(user_id, RedAlertDeportationResult::Deported),
                     Err(err) => result.insert(user_id, RedAlertDeportationResult::Error(err)),
                 };
             } else {
